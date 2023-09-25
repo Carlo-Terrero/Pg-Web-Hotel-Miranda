603193145c1867a7d953630579d3d0a2931119ca
Carlos Terrero <carlosjose2111@gmail.com>
2023-09-25 11:06:34 +0200

    changes

--- script/connectionApi.js
+++ script/connectionApi.js
@@ -1,16 +1,103 @@
 const myHeaders = new Headers();
 
+const section = document.querySelector('#container-rooms');
+const left = document.querySelector('#btn-left');
+const rigt = document.querySelector('#btn-rigt');
+// const a = document.getElementById('#select_element');
 
 const urlencoded = new URLSearchParams();
 
 const requestOptions = {
   method: 'GET',
-  headers: myHeaders,
-//   body: urlencoded,
-//   redirect: 'follow'
+  headers: myHeaders
 };
 
-fetch("https://hotel-miranda.onrender.com/rooms", requestOptions)
+function renderElement(element){
+  return document.createElement(element);
+}
+
+fetch("https://hotel-miranda.onrender.com/public/rooms", requestOptions)
+// fetch("http://localhost:3001/public/rooms", requestOptions)
   .then(response => response.json())
-  .then(result => console.log(result.rooms))
+  .then(result => {
+    console.log(result.rooms)
+    renderRooms(result.rooms);
+  })
   .catch(error => console.log('error', error));
+
+function renderRooms(rooms){
+  const itemsString = createRoomsItems(rooms).join('');
+  section.innerHTML = itemsString
+}
+
+function selectRoom(){
+  console.log('room -> ')
+}
+
+
+const createRoomsItems = rooms => rooms.map( room => {
+  return `
+    <div class="room">
+
+      <img 
+        src=${room.foto} 
+        class="img_room"
+        alt="img room" 
+      >
+    
+      <div class="container-pgn">
+        <div>
+          <img src="./assets/svg/8725460_bed_icon 1.png" alt="">            
+        </div>
+
+        <div>
+          <img src="./assets/svg/925808_wifi_icon 1.png" alt="">
+        </div>
+
+        <div>
+          <img src="./assets/svg/4920723_automobile_car_transport_transportation_travel_icon 1.png" alt="">            
+        </div>
+
+        <div>
+          <img src="./assets/svg/384878_cold_new year_snowflake_wheather_winter_icon 1.png" alt="">
+        </div>
+
+        <div>
+          <img src="./assets/svg/9042522_gym_icon 1.png" alt="">
+        </div>
+
+        <div>
+          <img src="./assets/svg/9081473_smoking_no_icon 1.png" alt="">
+        </div>
+        
+        <div>
+          <img src="./assets/svg/6623006_cocktail_drink_holidays_summer_vacation_icon 1.png" alt="">
+        </div>            
+        
+      </div>
+
+      <div class="container-detail">
+
+        <h1 class="cabecera-detail">
+            ${room.bed_type}
+        </h1>
+
+        <p>
+            ${room.description}
+        </p>
+
+        <div class="adquire-room">
+          <p class="precio-detail">
+            $${room.price}/Night
+          </p>
+
+          <button class="btn-adquire" id="select_element" onclick="() => console.log("klk")">
+              <strong> Booking Now </strong>
+          </button>
+        </div>
+
+      </div>
+    </div>
+  `
+  // console.log(room._id)
+})
--- script/slider/sliderHand.js
+++ script/slider/sliderHand.js
@@ -1,5 +1,6 @@
 class SliderHand{
     containerImg = document.getElementById('test');
+    containerImgCorredor = document.getElementById('container_corredor_img');
     timer;
     index = 0;
 
@@ -17,9 +18,9 @@ class SliderHand{
 
     constructor(){
         this.img = [
-            "https://images.unsplash.com/photo-1578683010236-d716f9a3f461?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMDY3NzJ8MHwxfHNlYXJjaHwzNnx8cm9vbSUyMGhvdGVsfGVufDB8fHx8MTY3Mzc5MjMzMg&ixlib=rb-4.0.3&q=80&w=400",
-            "https://images.unsplash.com/photo-1594563703937-fdc640497dcd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMDY3NzJ8MHwxfHNlYXJjaHw1fHxyb29tJTIwaG90ZWx8ZW58MHx8fHwxNjczNzkxNzYx&ixlib=rb-4.0.3&q=80&w=400",
-            "https://images.unsplash.com/photo-1568495248636-6432b97bd949?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMDY3NzJ8MHwxfHNlYXJjaHwyMzR8fHJvb20lMjBob3RlbHxlbnwwfHx8fDE2NzM3OTMxNjk&ixlib=rb-4.0.3&q=80&w=400"
+            "https://images.unsplash.com/photo-1578683010236-d716f9a3f461?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMDY3NzJ8MHwxfHNlYXJjaHwzNnx8cm9vbSUyMGhvdGVsfGVufDB8fHx8MTY3Mzc5MjMzMg&ixlib=rb-4.0.3&q=80&",
+            "https://images.unsplash.com/photo-1594563703937-fdc640497dcd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMDY3NzJ8MHwxfHNlYXJjaHw1fHxyb29tJTIwaG90ZWx8ZW58MHx8fHwxNjczNzkxNzYx&ixlib=rb-4.0.3&q=80&",
+            "https://images.unsplash.com/photo-1568495248636-6432b97bd949?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMDY3NzJ8MHwxfHNlYXJjaHwyMzR8fHJvb20lMjBob3RlbHxlbnwwfHx8fDE2NzM3OTMxNjk&ixlib=rb-4.0.3&q=80&"
         ]
 
         this.handleChange();
@@ -27,6 +28,7 @@ class SliderHand{
 
     handleChange(){
         this.containerImg.src = this.img[this.index];
+        this.containerImgCorredor.style.backgroundImage = `url(${this.img[this.index]})`
         this.index ++;
         this.handleTimeChange();
         if(this.index >= this.img.length) this.index = 0;
